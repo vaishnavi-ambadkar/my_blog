4798b9947a82e11d65258c8a3319f2e4371ed50f
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-30 16:38:25 +0530

    Update server.js

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -73,7 +73,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://my-blog-frontend-gamma.vercel.app'], // Allow local and deployed frontend
+  credentials: true, // Allow cookies if needed
+}));
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
 
